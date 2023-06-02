77d44ed4b4e7f7756c491dc0feaf80eed19f120d
QuentinJoanon <quentin.joanon@gmail.com>
2023-06-02 21:06:34 +0200

    Add state for searchbar value

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Theme } from '@lumx/react';
 import { mdiMagnify } from '@lumx/icons';
 
-const Search = () => (
-	<TextField theme={Theme.dark} placeholder="Search ..." icon={mdiMagnify} onChange={() => {}} />
-);
+
+const Search = () => {
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (value) => {
+    console.log(value);
+    setSearch(value);
+  };
+
+  return (
+	<TextField
+		theme={Theme.dark}
+		placeholder="Search ..."
+		icon={mdiMagnify}
+		onChange={handleSearch}
+		value={search}
+	/>
+  );
+};
 
 export default Search;
