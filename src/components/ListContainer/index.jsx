9555f83b31ca49641bef6fc932ba4716bc26843e
Hugo Aragones <hugo@qlower.com>
2023-10-24 13:12:28 +0200

    Added placeholder to avoid API restriction

--- src/components/ListContainer/index.jsx
+++ src/components/ListContainer/index.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import useFetch from "../../hooks/useFetch";
 import calculatePageNumber from "../../api/calculatePageNumber";
 import CharacterCard from "../CharacterCard";
+import placeholder from '../../api/placeholder'
 
 const ListContainer = () => {
-  const { data, loading, error } = useFetch("/characters");
-  !loading && error && console.error(error)
-  !loading && console.log('Pages number: ', calculatePageNumber(data.response.data.data.total));
+  const data = placeholder
+  const loading = false
+  const error = null
+  // const { data, loading, error } = useFetch("/characters");
 
-  return loading ? (
-    <p>Loading...</p> //TO DO: Switch to a real loading component
-  ) : (
+  !loading && error && console.error(error)
+  !loading && console.log('Pages number: ', calculatePageNumber(data?.response?.data?.data.total));
+
+  return !loading && !error ? (
     <div className="list-container">
-      {data.response.data.data.results.map((item) => (
+      {data?.response?.data?.data.results.map((item) => (
         <CharacterCard key={item.id} character={item} />
       ))}
     </div>
+    ) : (
+    <p>Loading...</p> //TO DO: Switch to a real loading component
   );
 };
 
